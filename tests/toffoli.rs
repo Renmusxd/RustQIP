9746983c8f8e970142b8535510d79ef814b3cb35
Sumner Hearth <sumnernh@gmail.com>
2021-09-01 16:33:53 -0400

    Toffoli test code cleanup

--- tests/toffoli.rs
+++ tests/toffoli.rs
@@ -1,8 +1,8 @@
 use qip::*;
 
 // https://en.wikipedia.org/wiki/Quantum_logic_gate#Toffoli_(CCNOT)_gate
-fn truth_table() -> Vec<((u64, u64, u64), (u64, u64, u64))> {
-    vec![
+const fn truth_table() -> [((u64, u64, u64), (u64, u64, u64)); 8] {
+    [
         ((0, 0, 0), (0, 0, 0)),
         ((0, 0, 1), (0, 0, 1)),
         ((0, 1, 0), (0, 1, 0)),
@@ -16,7 +16,7 @@ fn truth_table() -> Vec<((u64, u64, u64), (u64, u64, u64))> {
 
 #[test]
 fn test_toffoli() -> Result<(), CircuitError> {
-    for line in truth_table() {
+    for (input, output) in truth_table() {
         // create a builder
         let mut b = OpBuilder::new();
 
@@ -26,13 +26,13 @@ fn test_toffoli() -> Result<(), CircuitError> {
         let mut q3 = b.qubit();
 
         // change inputs as truth table
-        if line.0 .0 == 1 {
+        if input.0 == 1 {
             q1 = b.not(q1);
         }
-        if line.0 .1 == 1 {
+        if input.1 == 1 {
             q2 = b.not(q2);
         }
-        if line.0 .2 == 1 {
+        if input.2 == 1 {
             q3 = b.not(q3);
         }
 
@@ -55,9 +55,9 @@ fn test_toffoli() -> Result<(), CircuitError> {
         let m3 = measured3.get_measurement(&handle3).unwrap();
 
         // compare results with truth table outputs
-        assert_eq!(m1.0, line.1 .0);
-        assert_eq!(m2.0, line.1 .1);
-        assert_eq!(m3.0, line.1 .2);
+        assert_eq!(m1.0, output.0);
+        assert_eq!(m2.0, output.1);
+        assert_eq!(m3.0, output.2);
     }
 
     Ok(())
