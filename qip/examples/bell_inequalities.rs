cd008c596c7f3cccf832176ddce160c01e1b50a1
Kris <kristian_dilov@abv.bg>
2022-07-16 01:04:06 +0300

    Examples PR

--- qip/examples/bell_inequalities.rs
+++ qip/examples/bell_inequalities.rs
@@ -1,12 +1,32 @@
 use qip::prelude::*;
+#[cfg(feature = "macros")]
+use qip_macros::*;
 use std::num::NonZeroUsize;
 
+#[cfg(feature = "macros")]
 fn circuit1() -> Vec<f64> {
-
     let mut b = LocalBuilder::<f64>::default();
     let n = NonZeroUsize::new(2).unwrap();
 
     let q = b.qubit();
     let ra = b.register(n);
-    let r = program!() //Not sure what to use here
+    let r = program!(&mut b, r;
+        not r;
+        h r[0];
+        control not r[0], r[1];
+        rz(std::f64::consts::FRAC_PI_3) r[1];
+        h r;
+    )
+    .unwrap();
+    let (r, m_handle) = b.stochastic_measure(r);
+
+    //run and get probabilities
+    let (_, mut measured) = run_local::<f64>(&r).unwrap();
+    measured.pop_stochastic_measurements(m_handle).unwrap()
 }
+
+
+
+#[cfg(not(feature = "macros"))]
+fn main() {}
+
