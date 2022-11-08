c88f61994156462cf707e2d751ca30b8ac4cb8f1
Sumner Hearth <sumnernh@gmail.com>
2022-11-08 14:29:22 -0500

    Update coverage

--- coverage.sh
+++ coverage.sh
@@ -1,8 +1,8 @@
 export CARGO_INCREMENTAL=0
-export RUSTFLAGS="-Zprofile -Ccodegen-units=1 -Copt-level=0 -Clink-dead-code -Coverflow-checks=off -Zpanic_abort_tests -Cpanic=abort"
-export RUSTDOCFLAGS="-Cpanic=abort"
+export RUSTFLAGS="-Zprofile -Ccodegen-units=1 -Copt-level=0 -Clink-dead-code -Coverflow-checks=off -Cinstrument-coverage"
+export LLVM_PROFILE_FILE="qip-%p-%m.profraw"
 
 cargo clean
-cargo test
+cargo +nightly test
 
 grcov ./target/debug/ -s . -t html --llvm --branch --ignore-not-existing -o ./target/debug/coverage/
