ca93548de9caf5b0a6f2429435a6ef9550458fbd
Markus Mayer <widemeadows@gmail.com>
2023-09-10 01:55:36 +0200

    Implement Error for CircuitError

--- qip/src/errors.rs
+++ qip/src/errors.rs
@@ -1,19 +1,32 @@
+use std::error::Error;
+use std::fmt::{Display, Formatter};
+
 /// An error from building the circuit.
 #[derive(Debug)]
-pub struct CircuitError {
-    /// The error message.
-    pub msg: String,
+pub enum CircuitError {
+    /// A generic error.
+    Generic(String),
 }
 
 impl CircuitError {
     /// Construct a new error.
     pub fn new<S>(msg: S) -> Self
-        where
-            S: Into<String>,
+    where
+        S: Into<String>,
     {
-        Self { msg: msg.into() }
+        Self::Generic(msg.into())
     }
 }
 
 /// A result which may contain a circuit error.
 pub type CircuitResult<T> = Result<T, CircuitError>;
+
+impl Error for CircuitError {}
+
+impl Display for CircuitError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Generic(msg) => write!(f, "{}", msg),
+        }
+    }
+}
